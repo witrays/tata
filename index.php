ae0f8921d85adb204600448db2fd8e924ba269d1
witrays <33268550+witrays@users.noreply.github.com>
2018-05-13 06:52:41 +0800

    index.php

--- index.php
+++ index.php
@@ -3,7 +3,5 @@
  * Created by PhpStorm.
  * User: Administrator
  * Date: 2018/5/13
- * Time: 5:52
+ * Time: 6:52
  */
-
-echo phpinfo();
--- theme.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2018/5/13
- * Time: 6:14
- */
-echo "this is 'theme' branch";
